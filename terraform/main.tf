7a7c1e5e5c4499faa309048426b10ef8b1016cb0
Владислав Лесник <zerodistance@sirius.online>
2023-04-22 10:53:34 +0000

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,3 +123,61 @@ resource "yandex_vpc_security_group" "k8s-public-services" {
     to_port        = 65535
   }
 }
+
+############   Create cluster node group ############
+
+resource "yandex_kubernetes_node_group" "momo-group" {
+  cluster_id  = yandex_kubernetes_cluster.k8s-corpsehead.id
+  name        = "momo-group"
+  description = "momo-group"
+  version     = "1.22"
+
+  labels = {
+    "app" = "momo-store"
+  }
+
+  instance_template {
+    platform_id = "standard-v3"
+
+    network_interface {
+      nat        = false
+      subnet_ids = ["${yandex_vpc_subnet.mysubnet.id}"]
+    }
+
+    resources {
+      memory        = 2
+      cores         = 2
+      core_fraction = 50
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 32
+    }
+
+    scheduling_policy {
+      preemptible = false
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = "ru-central1-a"
+    }
+  }
+
+  maintenance_policy {
+    auto_upgrade = false
+    auto_repair  = false
+  }
+}
