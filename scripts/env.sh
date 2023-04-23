6537caf52c99ccd6022db49d376b6ee8cd45667d
Владислав Лесник <zerodistance@sirius.online>
2023-04-23 09:45:16 +0000

    Update scripts/env.sh, terraform/.gitlab-ci.yml

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-apk update && apk add --no-cache nano curl wget unzip bash jq openssh-client
+apk update && apk add --no-cache \
+  curl git wget unzip bash jq openssh-client
 
-wget -q https://hashicorp-releases.yandexcloud.net/terraform/1.4.0/terraform_1.4.0_linux_amd64.zip &&\
-  unzip terraform_1.4.0_linux_amd64.zip &&\
-  mv terraform /usr/local/bin 
+wget -q https://hashicorp-releases.yandexcloud.net/terraform/1.3.3/terraform_1.3.3_linux_amd64.zip &&\
+  unzip terraform_1.3.3_linux_amd64.zip &&\
+  mv ./terraform /usr/local/bin 
 
 curl -sLO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &&\
   chmod +x ./kubectl &&\
