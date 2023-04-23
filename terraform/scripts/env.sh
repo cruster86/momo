37fe04d6c057224c2f93daed54909dda81d68f39
Владислав Лесник <zerodistance@sirius.online>
2023-04-23 09:26:39 +0000

    Update terraform/scripts/env.sh

--- terraform/scripts/env.sh
+++ terraform/scripts/env.sh
@@ -15,5 +15,5 @@ mv /root/yandex-cloud/bin/yc /usr/local/bin
 
 wget https://get.helm.sh/helm-v3.11.0-linux-amd64.tar.gz &&\
 tar xzvf helm-v3.11.0-linux-amd64.tar.gz &&\
-chmod +x linux-amd64/helm \
+chmod +x linux-amd64/helm &&\
 mv linux-amd64/helm /usr/bin/helm
