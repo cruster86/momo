6c3ee40c0fae7efee825d08e7ff796274dac5560
Владислав Лесник <zerodistance@sirius.online>
2023-04-25 12:15:37 +0000

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,8 @@ resource "yandex_dns_recordset" "rs1" {
   zone_id = yandex_dns_zone.my-zone.id
   name    = "momo-store.corpsehead.space."
   type    = "A"
-  ttl     = 200
+  ttl     = 300
+  data    = "${yandex_kubernetes_cluster.k8s-corpsehead.master.0.external_v4_address}
 }
 
 resource "yandex_vpc_subnet" "mysubnet" {
