ec749a972508c64b17fa2f555c04be20693d6ba2
Владислав Лесник <zerodistance@sirius.online>
2023-04-27 10:56:29 +0000

    Update terraform/scripts/deploy.sh

--- terraform/scripts/deploy.sh
+++ terraform/scripts/deploy.sh
@@ -237,7 +237,7 @@ END
 
 ################   DEPLOY HELM MOMO-STORE   ################
 
-#echo "${NEXUS_REPO_PASS}" | helm repo add nexus https://nexus.k8s.praktikum-services.tech/repository/momo-store-vladislav-lesnik-helm/ --username ${NEXUS_REPO_USER} --password-stdin
+echo "${NEXUS_REPO_PASS}" | helm repo add nexus https://nexus.k8s.praktikum-services.tech/repository/momo-store-vladislav-lesnik-helm/ --username ${NEXUS_REPO_USER} --password-stdin
 
 #helm repo update nexus
 #helm upgrade --install momo-store nexus/momo-store \
