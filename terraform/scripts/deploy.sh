0cd0a2bac727a41693d97885f3aec0b90f7b695c
Владислав Лесник <zerodistance@sirius.online>
2023-04-27 09:50:57 +0000

    Update terraform/scripts/deploy.sh

--- terraform/scripts/deploy.sh
+++ terraform/scripts/deploy.sh
@@ -89,15 +89,6 @@ spec:
       labels:
         app: momo-store-backend
     spec:
-      affinity:
-        nodeAffinity:
-          requiredDuringSchedulingIgnoredDuringExecution:
-            nodeSelectorTerms:
-            - matchExpressions:
-              - key: app
-                operator: In
-                values:
-                - momo-store
       containers:
         - name: momo-store-backend
           image: gitlab.praktikum-services.ru:5050/zerodistance/momo-store/momo-store-backend:v1.0.1
