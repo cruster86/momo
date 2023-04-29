b6026b0d84d012215937f4c098187bef3d2dfa5b
Владислав Лесник <zerodistance@sirius.online>
2023-04-29 20:36:14 +0000

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,64 +145,64 @@ resource "yandex_vpc_subnet" "mysubnet" {
 
 ##############    Create Security Groups    ##############
 
-resource "yandex_vpc_security_group" "k8s-master-whitelist" {
-  name        = "k8s-master-whitelist"
-  description = "Group rules allow access to the Kubernetes API from the internet. Apply the rules to the cluster only."
-  network_id  = yandex_vpc_network.mynet.id
+#resource "yandex_vpc_security_group" "k8s-master-whitelist" {
+#  name        = "k8s-master-whitelist"
+#  description = "Group rules allow access to the Kubernetes API from the internet. Apply the rules to the cluster only."
+#  network_id  = yandex_vpc_network.mynet.id
+#
+#  ingress {
+#    protocol       = "TCP"
+#    description    = "Rule allows connections to the Kubernetes API via port 6443 from a specified network."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 6443
+#  }
+#
+#  ingress {
+#    protocol       = "TCP"
+#    description    = "Rule allows connections to the Kubernetes API via port 443 from a specified network."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 443
+#  }
+#}
 
-  ingress {
-    protocol       = "TCP"
-    description    = "Rule allows connections to the Kubernetes API via port 6443 from a specified network."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 6443
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Rule allows connections to the Kubernetes API via port 443 from a specified network."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 443
-  }
-}
-
-resource "yandex_vpc_security_group" "k8s-public-services" {
-  name        = "k8s-public-services"
-  description = "Group rules allow connections to services from the internet. Apply the rules only for node groups."
-  network_id  = yandex_vpc_network.mynet.id
-  ingress {
-    protocol          = "TCP"
-    description       = "Rule allows availability checks from load balancer's address range. It is required for the operation of a fault-tolerant cluster and load balancer services."
-    predefined_target = "loadbalancer_healthchecks"
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ANY"
-    description       = "Rule allows master-node and node-node communication inside a security group."
-    predefined_target = "self_security_group"
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol       = "ICMP"
-    description    = "Rule allows debugging ICMP packets from internal subnets."
-    v4_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-  }
-  ingress {
-    protocol       = "TCP"
-    description    = "Rule allows incoming traffic from the internet to the NodePort port range. Add ports or change existing ones to the required ports."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 30000
-    to_port        = 32767
-  }
-  egress {
-    protocol       = "ANY"
-    description    = "Rule allows all outgoing traffic. Nodes can connect to Yandex Container Registry, Yandex Object Storage, Docker Hub, and so on."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
-}
+#resource "yandex_vpc_security_group" "k8s-public-services" {
+#  name        = "k8s-public-services"
+#  description = "Group rules allow connections to services from the internet. Apply the rules only for node groups."
+#  network_id  = yandex_vpc_network.mynet.id
+#  ingress {
+#    protocol          = "TCP"
+#    description       = "Rule allows availability checks from load balancer's address range. It is required for the #operation of a fault-tolerant cluster and load balancer services."
+#    predefined_target = "loadbalancer_healthchecks"
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#  ingress {
+#    protocol          = "ANY"
+#    description       = "Rule allows master-node and node-node communication inside a security group."
+#    predefined_target = "self_security_group"
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#  ingress {
+#    protocol       = "ICMP"
+#    description    = "Rule allows debugging ICMP packets from internal subnets."
+#    v4_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+#  }
+#  ingress {
+#    protocol       = "TCP"
+#    description    = "Rule allows incoming traffic from the internet to the NodePort port range. Add ports or change #existing ones to the required ports."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    from_port      = 30000
+#    to_port        = 32767
+#  }
+#  egress {
+#    protocol       = "ANY"
+#    description    = "Rule allows all outgoing traffic. Nodes can connect to Yandex Container Registry, Yandex Object #Storage, Docker Hub, and so on."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    from_port      = 0
+#    to_port        = 65535
+#  }
+#}
 
 ##############    Create Cluster Node Group    ##############
 
@@ -222,10 +222,10 @@ resource "yandex_kubernetes_node_group" "momo-group" {
     network_interface {
       nat        = true
       subnet_ids = ["${yandex_vpc_subnet.mysubnet.id}"]
-      security_group_ids = [
-        yandex_vpc_security_group.k8s-public-services.id,
-        yandex_vpc_security_group.k8s-master-whitelist.id
-      ]
+#      security_group_ids = [
+#        yandex_vpc_security_group.k8s-public-services.id,
+#        yandex_vpc_security_group.k8s-master-whitelist.id
+#      ]
     }
 
     resources {
