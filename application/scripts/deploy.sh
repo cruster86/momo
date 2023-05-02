76e723e8e719544114847ca8e75330725cbcab82
Владислав Лесник <zerodistance@sirius.online>
2023-05-02 10:31:13 +0000

    Update packages/.gitlab-ci.yml, application/.gitlab-ci.yml, application/scripts/deploy.sh, backend/.gitlab-ci.yml, frontend/.gitlab-ci.yml, infrastructure/.gitlab-ci.yml

--- application/scripts/deploy.sh
+++ application/scripts/deploy.sh
@@ -8,11 +8,11 @@ echo ${NEXUS_REPO_PASS} | helm repo add nexus ${NEXUS_HELM_REPO} --username ${NE
 helm repo update nexus
 helm upgrade --install momo-store nexus/momo-store \
   --namespace momo-store --create-namespace \
-  --set global.tag="v1.0.5" \
+  --set global.tag="${TAG}" \
   --set global.backServiceName=momo-store-backend --set global.backServicePort=8081 \
   --atomic --wait
 
-# --set global.tag="${TAG}"
+# --set global.tag="v1.0.5"
 
 ################   SHOW INGRESS CONTROLLER IP   #################
 
